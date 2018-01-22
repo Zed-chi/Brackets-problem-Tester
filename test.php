bdb90ed1d3e90a72c9743c8114623d542dbc1263
AlexE <zed-chi@mail.ru>
2018-01-22 11:08:28 +0300

    file check
    ---

--- test.php
+++ test.php
@@ -3,11 +3,19 @@ require __DIR__ . '/vendor/autoload.php';
 
 $x=$argv[1];
 
-try{
-	$data = file_get_contents($x);
-	$b = new Zedchi\Verify();
-	echo $b->verify($data);
+$x = $argv[1];
+
+try {
+    if(!is_file($x)){throw new \Exception("not A file");}
+    try {
+        $data = file_get_contents($x);
+        $item = new Zedchi\Verify();
+        echo $item->verify($data) . "\n";
+    }
+    catch (Exception $e){
+        echo $e->getMessage()."\n";
+    }
 }
 catch(Exception $e){
-	echo $e->getMessage();
+    echo $e->getMessage()."\n";
 }
